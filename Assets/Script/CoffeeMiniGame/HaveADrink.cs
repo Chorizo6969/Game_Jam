effe1126d8a55ce4f9faadf295fde2b684990848
Eva291204 <eva.fricaud@e-artsup.net>
2023-12-21 16:56:03 +0100

    Set up café

--- Assets/Script/CoffeeMiniGame/HaveADrink.cs
+++ Assets/Script/CoffeeMiniGame/HaveADrink.cs
@@ -5,36 +5,40 @@ public class HaveADrink : Interactable
 {
     protected override void Interact()
     {
-        BarManager.Instance.drink = true;
-
-        if (BarManager.Instance.player.tag == "coffee" && gameObject.tag == "coffee")
+        if (BarManager.Instance.handFull)
         {
-            BarManager.Instance.HappyClient();
-            Debug.Log("café donner");
+            BarManager.Instance.drink = true;
+
+            if (BarManager.Instance.player.tag == "coffee" && gameObject.tag == "coffee")
+            {
+                BarManager.Instance.HappyClient();
+                Debug.Log("café donner");
+            }
+
+            if (BarManager.Instance.player.tag == "tea" && gameObject.tag == "tea")
+            {
+                BarManager.Instance.HappyClient();
+                Debug.Log("thé donner");
+            }
+
+            if (BarManager.Instance.player.tag == "tea" && gameObject.tag == "coffee") //une else fait bug
+            {
+                BarManager.Instance.AngryClient();
+                Debug.Log("boissoon donner");
+            }
+
+            if (BarManager.Instance.player.tag == "coffee" && gameObject.tag == "tea") //une else fait bug
+            {
+                BarManager.Instance.AngryClient();
+                Debug.Log("boissoon donner");
+            }
+
+            gameObject.tag = "Untagged";
+            BarManager.Instance.handFull = false;
+
+            StartCoroutine(Drink());
         }
 
-        if (BarManager.Instance.player.tag == "tea" && gameObject.tag == "tea")
-        {
-            BarManager.Instance.HappyClient();
-            Debug.Log("thé donner");
-        }
-
-        if(BarManager.Instance.player.tag == "tea" && gameObject.tag == "coffee") //une else fait bug
-        {
-            BarManager.Instance.AngryClient();
-            Debug.Log("boissoon donner");
-        }
-        
-        if(BarManager.Instance.player.tag == "coffee" && gameObject.tag == "tea") //une else fait bug
-        {
-            BarManager.Instance.AngryClient();
-            Debug.Log("boissoon donner");
-        }
-
-        gameObject.tag = "Untagged";
-        BarManager.Instance.handFull = false;
-
-        StartCoroutine(Drink());
     }
     IEnumerator Drink()
     {
